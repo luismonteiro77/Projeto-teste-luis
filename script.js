d5670f1c858df2ec74828d6d0c5d819a8c1dcd84
Seu Nome <seu.email@exemplo.com>
2025-04-06 12:09:01 +0100

    Adicionando arquivos básicos do site

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log('Site carregado com sucesso!');
+
+// Exemplo de função
+function bemVindo() {
+    alert('Bem-vindo ao meu site!');
+}
+
+// Chamando a função quando a página carregar
+window.onload = bemVindo; 
